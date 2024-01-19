c14f31bb06a4ca2b914a5fb5f589960fe71f7e0e
natashaval <natasha.valentina1998@gmail.com>
2024-01-19 21:46:48 +0000

    section 2 part 2 passing function as parameter

--- Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -7,7 +7,9 @@ bool InsideUnitSphere(double x, double y, double z)
     return ((x*x + y*y + z*z) <= 1);
 }
 
-double IntegrateMonteCarlo3D(int n_points, double min, double max)
+// pass a function as parameter https://www.codingame.com/playgrounds/15869/c-runnable-snippets/passing-a-function-as-parameter
+double IntegrateMonteCarlo3D(int n_points, double min, double max, 
+    std::function<bool(double, double, double)> isInsideUnitSphere)
 {
     int count = 0;
     // N.B. std::pow with integer arguments can be less efficient than
@@ -26,7 +28,7 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
         double x = distribution(rng_mt);
         double y = distribution(rng_mt);
         double z = distribution(rng_mt);
-        if(InsideUnitSphere(x, y, z)) count++;
+        if(isInsideUnitSphere(x, y, z)) count++;
     }
 
     return static_cast<double>(count) / n_points * VolCube;
@@ -37,7 +39,8 @@ int main()
 {
     int N_points = 10000;
     
-    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
+    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0,
+    &InsideUnitSphere);
 
     std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
 }
