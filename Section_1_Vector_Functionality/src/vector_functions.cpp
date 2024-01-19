d6b9b3a5b5ab717b32156caaa5f5fcdcba3e0bd6
natashaval <natasha.valentina1998@gmail.com>
2024-01-19 14:50:08 +0000

    exercise 1 part 2

--- Section_1_Vector_Functionality/src/vector_functions.cpp
+++ Section_1_Vector_Functionality/src/vector_functions.cpp
@@ -6,6 +6,7 @@
 
 int countMultiplesOfFive(std::vector<int> v)
 {
+    /*
     int count = 0;
     for (int i = 0; i < v.size(); i++) {
         if (v.at(i) % 5 == 0) {
@@ -13,12 +14,20 @@ int countMultiplesOfFive(std::vector<int> v)
         }
     }
     return count;
-}
+    */
 
+    // why `auto` as typedef? because lamda function doesn't actually have a type -> therefore annointing it as auto tells the compiler to skip the typedef check
+    auto isDivisibleByFive = [](int num) -> bool { return num % 5 == 0; };
+    int count = std::count_if(v.begin(), v.end(), isDivisibleByFive);
+    return count;
+
+    // inline function
+    // return std::count_if(v.begin(), v.end(), [](int num) -> bool { return num % 5 == 0; });
+}
 
 void addElements(std::vector<int> v, int x, int n)
 {
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         v.push_back(x);
     }
